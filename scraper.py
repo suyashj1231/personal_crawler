25b0b95b025bfd17a77fbe148f92992ebc4ac4ec
Suyash Jain <suyashj@uci.edu>
2025-02-14 14:36:52 -0800

    tweaked page size limit and pps exception

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from tokenizer import tokenize
 from collections import deque
 from simhash_basic import make_simhash, simhash_diff
 MIN_WORD_COUNT = 50
-MAX_PAGE_SIZE = 4_000_000
+MAX_PAGE_SIZE = 1 * 1024 * 1024  # 1MB = 1 * 1024 * 1024 bytes
 # Global storage for tracking unique URLs, subdomains, and word frequency
 visited_urls = set()
 visited_hashes = set()
@@ -17,8 +17,10 @@ word_counts = {}
 longest_page = (None, 0)  # (URL, word count)
 url_queue = deque()
 robots_parsers = {}
+
 STOPWORDS = set("""
 a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves""".split())
+
 TRAP_PATTERNS = [
     r'\?sort=', r'\?order=', r'\?page=', r'\?date=', r'\?filter=', r'calendar', r'\?view=',
     r'\?session=', r'\?print=', r'\?lang=', r'\?mode=', r'\?year=', r'\?month=', r'\?day=', r'\?.ical$'
@@ -149,7 +151,8 @@ def is_valid(url):
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
                 + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ical)$", parsed.path.lower()):
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ical|djvu|apk|bak|tmp|jpg|jpeg|svg|pps)$",
+                parsed.path.lower()):
             return False
 
         return True
